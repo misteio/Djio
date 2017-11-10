91a9dc97ed43e1b515539255736d707982e82a22
MisTe <j@miste.io>
2017-11-10 01:53:56 +0100

    GrapesJs begin Integration
    on top of CKeditor

--- website/core/urls.py
+++ website/core/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     # post views
     url(r'^admin/password-change$', views.password_change, name='admin_user_password_change'),
     url(r'^admin/ckupload', csrf_exempt(views.ckupload_service), name='admin_ckeditor_upload'),
+    url(r'^admin/editor', views.editor_html, name='admin_ckeditor_upload'),
+    url(r'^admin/json-editor', views.json_editor, name='admin_ckeditor_upload'),
 ]
